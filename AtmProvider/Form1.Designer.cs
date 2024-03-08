b02d88969471b1a66fc117098e484789d3851696
r.dain24 <r.dain24@LAB108PC12>
2024-03-08 12:30:31 +0200

    Add project files.

---
+++ AtmProvider/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace AtmProvider
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            errorProvider1 = new ErrorProvider(components);
+            errorProvider2 = new ErrorProvider(components);
+            label1 = new Label();
+            label2 = new Label();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)errorProvider1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)errorProvider2).BeginInit();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(456, 143);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(150, 31);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(456, 204);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(150, 31);
+            textBox2.TabIndex = 1;
+            // 
+            // errorProvider1
+            // 
+            errorProvider1.ContainerControl = this;
+            // 
+            // errorProvider2
+            // 
+            errorProvider2.ContainerControl = this;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(288, 143);
+            label1.Name = "label1";
+            label1.Size = new Size(149, 25);
+            label1.TabIndex = 2;
+            label1.Text = "enter the amount";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(288, 210);
+            label2.Name = "label2";
+            label2.Size = new Size(37, 25);
+            label2.TabIndex = 3;
+            label2.Text = "pin";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(325, 305);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 4;
+            button1.Text = "withdraw";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)errorProvider1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)errorProvider2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private ErrorProvider errorProvider1;
+        private Label label1;
+        private ErrorProvider errorProvider2;
+        private Label label2;
+        private Button button1;
+    }
+}
---
+++ AtmProvider/Form1.cs
@@ -0,0 +1,43 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace AtmProvider
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(textBox1.Text, out int value))
+            {
+                errorProvider1.SetError(textBox1, "invalid email");
+            }
+            else
+            {
+                errorProvider1.SetError(textBox1, string.Empty);
+            }
+            if (textBox2.Text.Length != 4)
+            {
+                errorProvider2.SetError(textBox2, "too short");
+                if (!int.TryParse(textBox2.Text, out int value2))
+                {
+                    errorProvider2.SetError(textBox2, "invalid password");
+                }
+            } 
+            else
+            {
+                errorProvider2.SetError(textBox2, string.Empty);
+            }
+            if (errorProvider1.GetError(textBox1).Equals(string.Empty) && errorProvider2.GetError(textBox2).Equals(string.Empty))
+            {
+                MessageBox.Show("Thanks");
+            }
+            else {
+                MessageBox.Show(errorProvider1.GetError(textBox1) + Environment.NewLine + errorProvider2.GetError(textBox2));
+            }
+        }
+    }
+}
